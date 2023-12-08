45c533267f561ea9fc69f332af7f067d0a4b1903
Saksham Patil <sakshampatil815@gmail.com>
2023-12-08 14:31:33 +0530

    initial server setup

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const mongo = require("mongoose");
+
+const app = express();
+app.use(cors());
+
+//connecting to mongoose
+const connect = async () => {
+  try {
+    await mongo.connect(process.env.MONGO_CONNECT);
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connect();
+
+//starting server
+const port = process.env.PORT || 3001;
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else [console.log(`Server is running on PORT = ${port}`)];
+});
